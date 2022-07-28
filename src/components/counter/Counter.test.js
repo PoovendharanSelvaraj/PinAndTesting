15b500f64d44722916ba34b708c5d47791804311
Poovendharan Selvaraj <97455003+PoovendharanSelvaraj@users.noreply.github.com>
2022-07-28 19:41:03 +0530

    reduxCounter

--- src/components/counter/Counter.test.js
+++ src/components/counter/Counter.test.js
@@ -13,5 +13,15 @@ describe('Counter Component testing',()=>{
         let increment = screen.getByText("Increment")
         userEvent.click(increment);
         expect(h1).toHaveTextContent("1")
-    })
+    });
+
+    it('Counter substraction testing',()=>{
+        render(<Counter/>)
+        let h1 = screen.getByText("Counter: 0")
+        expect(h1).toHaveTextContent("0")
+
+        let increment = screen.getByText("Decrement")
+        userEvent.click(increment);
+        expect(h1).toHaveTextContent("-1")
+    });
 })
